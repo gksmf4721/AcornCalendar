8c5d20bf2aa90b1f78fd42845c1e561dea037981
DESKTOP-Q5QDU6L\user <gksmf4721@naver.com>
2023-06-11 14:44:32 +0900

    member.xml, MemberService.java 수정

--- src/main/java/acorn/calendar/com/member/service/MemberService.java
+++ src/main/java/acorn/calendar/com/member/service/MemberService.java
@@ -1,19 +1,22 @@
 package acorn.calendar.com.member.service;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acorn.calendar.config.data.AcornMap;
 import lombok.RequiredArgsConstructor;
 
-@Service
+@Service("memberService")
 @RequiredArgsConstructor
 public class MemberService {
 
-	private static SqlSession sqlSession;
+	@Autowired
+	private SqlSession sqlSession;
+
+	// java 버전으로 인해 autowired없이 private static으로 선언 불가
 	
-	public int insertMember(AcornMap acornMap) throws Exception {
-		int result = sqlSession.insert("mapper.com.member.insertMember",acornMap);
-		return result;
+	public void insertMember(AcornMap acornMap) throws Exception {
+		sqlSession.insert("mapper.com.member.insertMember",acornMap);
 	}
 }
