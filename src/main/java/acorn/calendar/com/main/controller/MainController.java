1113e66559feb93e6e39208cda83f095ecd65a09
DESKTOP-Q5QDU6L\user <gksmf4721@naver.com>
2023-06-14 11:00:13 +0900

    아이디 저장 기능 생성 (쿠키), MainController, service 생성, MemberController에서 로그인 성공 시, return url 수정

---
+++ src/main/java/acorn/calendar/com/main/controller/MainController.java
@@ -0,0 +1,38 @@
+package acorn.calendar.com.main.controller;
+
+import acorn.calendar.com.member.service.MemberService;
+import acorn.calendar.config.data.AcornMap;
+import acorn.calendar.config.model.LoginSession;
+import acorn.calendar.config.util.JsonUtils;
+import acorn.calendar.config.util.PasswordHashUtils;
+import acorn.calendar.config.util.ResponseUtils;
+import acorn.calendar.config.util.ValidateUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+//	@Autowired
+//	private MemberService memberService;
+//
+//	@Autowired
+//	private MessageSource messageSource;
+
+	@RequestMapping("/main.do")
+	public String login() throws Exception {
+		return "main";
+	}
+
+
+}
