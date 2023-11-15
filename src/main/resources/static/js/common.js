c1fd81ed15c04b1639a964c36093b32cf348c767
hyony590 <runa369@naver.com>
2023-11-15 16:26:53 +0900

    고장나있던 부분 대부분 해결!

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,93 +1,104 @@
-function strToObj(xhr){
-    let obj = JSON.parse(xhr.response);
-    return obj;
-}
+    //데이터 유효성 검사 
+    function fn_dataChk(formId){
+        let chk = true;
+        const allElements = formId.getElementsByTagName('*');
+        for (let i = 0; i < allElements.length; i++) {
+            const element = allElements[i];
+            console.log()
+            var type = element.getAttribute('type');    //유효성검사가 필요한 타입
+            var name = element.getAttribute('name');    //유효성검사가 필요한 이름
+            var className = element.getAttribute('class');
+            var id = element.getAttribute('id');
+            var chk_befo = $("."+className).data('dupl');
 
-//데이터 유효성 검사 
-function fn_dataChk(formId){
-    let chk = true;
-    const allElements = formId.getElementsByTagName('*');
-    for (let i = 0; i < allElements.length; i++) {
-        const element = allElements[i];
-        console.log()
-        var type = element.getAttribute('type');    //유효성검사가 필요한 타입
-        var name = element.getAttribute('name');    //유효성검사가 필요한 이름
-        var className = element.getAttribute('class');
-        var id = element.getAttribute('id');
-        var chk_befo = $("."+className).data('dupl');
-
-        if(element.getAttribute('readonly')==false &&element.getAttribute('disabled')==false || element.hasAttribute('data-name')){
-            if(type == 'checkbox'){
-                if(element.checked == false){
-                    $.alertError(element.dataset.name + '을(를) 체크해주세요');
-                    element.focus();
-                    chk = false;
-                    return false;
+            if(element.getAttribute('readonly')==false &&element.getAttribute('disabled')==false || element.hasAttribute('data-name')){
+                if(type == 'checkbox'){
+                    if(element.checked == false){
+                        $.alertError(element.dataset.name + '을(를) 체크해주세요');
+                        element.focus();
+                        chk = false;
+                        return false;
+                    }
+                } else if(type == 'password'){
+                    if (element.value =='') {
+                        $.alertError(element.dataset.name + '을(를) 입력해주세요');
+                        element.focus();
+                        chk = false;
+                        return false;
+                    }
+                } else if(type == 'radio'){
+                    if($("input:radio[name='" + name +"']:checked").length == 0){
+                        $.alertError(element.dataset.name + '을(를) 선택해주세요');
+                        element.focus();
+                        chk = false;
+                        return false;
+                    }
+                } else if(type == 'text' || type=='date'){
+                    if (element.value =='') {
+                        $.alertError(element.dataset.name + '을(를) 입력해주세요');
+                        element.focus();
+                        chk = false;
+                        return false;
+                    }
+                } else if(element == 'select'){ //select박스일경우
+                    if(element.value == ''){
+                        $.alertError(element.dataset.name + '을(를) 선택해주세요');
+                        element.focus();
+                        chk = false;
+                        return false;
+                    }
+                } else {
+                    if(element.value == ''){
+                        element.focus();
+                        $.alertError(element.dataset.name + '을(를) 입력해주세요');
+                        chk = false;
+                        return false;
+                    }
                 }
-            } else if(type == 'password'){
-                if (element.value =='') {
-                    $.alertError(element.dataset.name + '을(를) 입력해주세요');
-                    element.focus();
-                    chk = false;
-                    return false;
-                }
-            } else if(type == 'radio'){
-                if($("input:radio[name='" + name +"']:checked").length == 0){
-                    $.alertError(element.dataset.name + '을(를) 선택해주세요');
-                    element.focus();
-                    chk = false;
-                    return false;
-                }
-            } else if(type == 'text' || type=='date'){
-                if (element.value =='') {
-                    $.alertError(element.dataset.name + '을(를) 입력해주세요');
-                    element.focus();
-                    chk = false;
-                    return false;
-                }
-            } else if(element == 'select'){ //select박스일경우
-                if(element.value == ''){
-                    $.alertError(element.dataset.name + '을(를) 선택해주세요');
-                    element.focus();
-                    chk = false;
-                    return false;
-                }
-            } else {
-                if(element.value == ''){
-                    element.focus();
-                    $.alertError(element.dataset.name + '을(를) 입력해주세요');
-                    chk = false;
-                    return false;
-                }
+            } else if(chk_befo == 'N'){
+                $.alertError("중복 체크를 해주세요");
+                chk = false;
+                return false;
             }
-        } else if(chk_befo == 'N'){
-            $.alertError("중복 체크를 해주세요");
-            chk = false;
-            return false;
         }
+        return chk;
     }
-    return chk;
-}
 
+    //실패시 뜨는 alert
+    $.alertError = function(text){
+        swal({
+            text : text,
+            icon : "error",
+            button : "닫기",
+            closeOnClickOutside : false,
+        });
+    };
 
-$.alertError = function(text){
-    swal({
-        text : text,
-        icon : "error",
-        button : "닫기",
-    });
-};
+    //성공시 뜨는 alert
+    $.alertSuccess = function(text){
+        swal({
+            text : text,
+            icon : "success",
+            button : "닫기",
+            closeOnClickOutside : false,
+        });
+    }
 
-$.alertSuccess = function(text){
-    swal({
-        text : text,
-        icon : "success",
-        button : "닫기",
-    });
-}
+    //성공시 alert뜨고 난 후, return 받은 url로 보내는 alert
+    $.alertSuccessRtn = function(text, url){
+        swal({
+            text : text,
+            icon : "success",
+            button : "닫기",
+            closeOnClickOutside : false,
+        }).then(result =>{
+            window.location.href = url;
+        });
+    }
 
-$.confirm = function(){
+    //확인용 alert
+    $.confirm = function(){
 
-}
+    }
 
 
--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -1,14 +1,21 @@
     //URL 이동
     function forwardURL(url){
-        if(url === 'kakao'){
-            $.alertError("준비중입니다!");
-        } else if(url === 'findId'){
-            window.location.href = "/find.do?type=I";
-        } else if(url === 'findPw'){
-            window.location.href = "/find.do?type=P";
-        } else if(url === 'join'){
-            window.location.href = "/join.do";
+        var answer = "";
+        switch(url){
+            case 'findId':
+                answer = "/find.do?type=I";
+                break;
+            case 'findPw':
+                answer = "/find.do?type=P";
+                break;
+            case 'join':
+                answer = "/join.do";
+                break;
+            case 'login' || '/':
+                answer = "/";
+                break;
         }
+        return window.location.href = answer;
     }
 
     //로그인 화면 => 로그인 처리
@@ -17,15 +24,14 @@
         let inputPw = document.getElementById('mPw').value;
         let chk = document.getElementById('saveId').checked;
 
+        ajaxData = {mId:inputId, mPw:inputPw}
+
         if(fn_dataChk(formData)){
             $.ajax({
                 type : "POST",
                 contentType : "application/json",
                 url : '/loginCheck.json',
-                data : JSON.stringify({
-                    mId: inputId,
-                    mPw: inputPw
-                }),
+                data : JSON.stringify(ajaxData),
                 dataType : 'json',
                 success : function(rslt){
                     if(rslt.resultCd == 1){
@@ -36,6 +42,8 @@
                         }
                         sessionStorage.setItem("id",inputId);
                         location.href = rslt.resultUrl;
+                    }else{
+                        $.alertError(rslt.resultMsg);
                     }
                 },
                 error : function(request, status, error){
@@ -48,21 +56,23 @@
 	//아이디찾기&비밀번호찾기&회원가입 => 이메일 보내기
 	function sendMail(mtype){
 		let email = document.getElementById("mEmail").value;
+		if(email === ""){ $.alertError("이메일을 입력해주세요"); return;}
 
-		if(email === ""){
-			$.alertError("이메일을 입력해주세요"); return false;
-		}
+        ajaxData = {mEmail:email, type:mtype}
+
 		$.ajax({
 			type : "POST",
 			url : "/sendMailAuth.json",
 			dataType:'json',
 			contentType : "application/json",
-			data : JSON.stringify({
-                mEmail: email,
-                type: mtype
-            }),
+			data : JSON.stringify(ajaxData),
 			success : function(rslt){
 				if(rslt.resultCd == 1){
+                    if(mtype == 'J'){
+                        document.getElementById("mEmailChk").style.display = '';
+                        document.getElementById("confirmAuthBtn").style.display = '';
+                        document.getElementById("mEmail").readOnly = true;
+                    }
 					$.alertSuccess(rslt.resultMsg);
 				} else {
 					$.alertError(rslt.resultMsg);
@@ -71,37 +81,40 @@
 		});
 	}
 
-    //아이디_비밀번호_찾기 => 인증번호 확인
+    //아이디찾기&비밀번호찾기&회원가입 => 인증번호 확인
 	function confirmMail(mtype){
 		let email = document.getElementById("mEmail").value;
 		let emailChk = document.getElementById("mEmailChk").value;
+        let emailChk_info = document.getElementById("emailChk_info");
 
-		if(emailChk === ""){
-			$.alertError("인증번호를 입력해주세요"); return false;
-		}
+		if(emailChk === ""){ $.alertError("인증번호를 입력해주세요"); return;}
+
+        ajaxData = {mEmail:email, inputAuth:emailChk, type:mtype}
 
 		$.ajax({
 			type : "POST",
 			url : "/confirmMailAuth.json",
 			dataType:'json',
 			contentType : "application/json",
-			data : JSON.stringify({
-                mEmail : email ,
-                inputAuth : emailChk ,
-                type : mtype
-            }),
+			data : JSON.stringify(ajaxData),
 			success : function(rslt){
 				if(rslt.resultCd == 1){
                     if(mtype == 'I'){
-                        $.alertSuccess(rslt.resultMsg);
                         document.getElementById("form").style.display = 'none';
                         document.getElementById("result_div").style.display = 'block';
                         document.getElementById("result").innerText = rslt.resultData;
+                        document.getElementById("result").className = 'result_tag';
                     } else if (mtype == 'P'){
-                        $.alertSuccess(rslt.resultMsg);
                         document.getElementById("form").style.display = 'none';
                         document.getElementById("inputPw").style.display = 'block';
+                    } else if (mtype == 'J'){
+                        emailChk_info.dataset.dupl = 'Y';                       //이메일 인증 Y로 변경
+                        emailChk_info.style.display = 'none';                   //이메일 인증 문구 삭제
+                        document.getElementById("mEmailChk").readOnly = true;   //이메일 인증 readonly 처리
+                        document.getElementById("sendBtn").disabled = true;     //인증번호전송 버튼 diabled 처리
+                        document.getElementById("confirmAuthBtn").disabled = true;  //인증확인 버튼 disabled 처리
                     }
+                    $.alertSuccess(rslt.resultMsg);
 				}else{
 					$.alertError(rslt.resultMsg);
 				}
@@ -114,170 +127,57 @@
         let email = document.getElementById("mEmail").value;
         let pw = document.getElementById("mPw").value;
         let pwChk = document.getElementById("mPwChk").value;
-        if(pw == '' || pwChk == ''){
-            $.alertError("비밀번호를 확인해주세요"); return false;
+        if(pw == '' || pwChk == '' || pw != pwChk){
+            $.alertError("비밀번호를 확인해주세요"); return;
         }
+        ajaxData = {mPw:pw, mPwChk:pwChk, mEmail:email}
         $.ajax({
             type : "POST",
             url : "/pwModify.json",
             dataType:'json',
             contentType : "application/json",
-            data : JSON.stringify({
-                mPw : pw,
-                mPwChk : pwChk,
-                mEmail : email
-            }),
+            data : JSON.stringify(ajaxData),
             success : function(rslt){
                 if(rslt.resultCd == 1){
-                    $.alertSuccess(rslt.resultMsg);
-                    location.href=rslt.resultUrl;
+                    $.alertSuccessRtn(rslt.resultMsg, rslt.resultUrl);
                 } else {
                     $.alertError(rslt.resultMsg);
                 }
-                
             }
         });
     } 
 
-//회원가입 => 아이디, 닉네임 중복검사
-function duplicate(id, idName){
-    var chkVal = document.getElementById(id).value;
-    if(chkVal == ""){
-        $.alertError(idName + "을(를) 입력해주세요!! •̀ㅅ•́");
-        return false;
-    }
+    //회원가입 => 아이디, 닉네임 중복검사
+    function duplicate(id, idName, type){
+        var chkVal = document.getElementById(id).value;
+        var idChk_info = document.getElementById(idChk_info);
+        var nickChk_info = document.getElementById(nickChk_info);
 
-    $.ajax({
-        type : "POST",
-        contentType : "application/json",
-        url : '/inputCheck.json',
-        data : JSON.stringify({
-            type: idName, 
-            id: chkVal
-        }),
-        dataType : 'json',
-        success : function(rslt){
-            if(rslt.resultCd == 1){
-                if(rslt.type == 'id'){
-                    $.alertSuccess("아이디 중복 검사 완료");
-                    $("#idChk_info").data("dupl", "Y");
-                    $("#idChk_info").hide();
-                } else if(rslt.type == 'nickname'){
-                    $.alertSuccess("닉네임 중복 검사 완료");
-                    $("#nickChk_info").data("dupl", "Y");
-                    $("#nickChk_info").hide();
-                }
-            } else {
-                $.alertError("이미 사용하고 있어요. ʅ（◞‿◟）ʃ");
-            }
-        },
-        error : function(request, status, error){
-            $.alertError("javaScript error : "+ error + "request :" + request + "status : " + status);
-        }
-    });
-}
-
-//회원가입 => 중복체크 후, 값 변경되면 다시 중복체크
-function inputChk(type){
-    if(type == 'idDupl'){
-        $("#idChk_info").show();
-        $("#idChk_info").data("dupl", "N");
-    } else if(type == 'nick'){
-        $("#nickChk_info").show();
-        $("#nickChk_info").data("dupl", "N");
-    };
-}
-
-// //이메일 보내기
-// function sendMail(){
-//     let email = $("#mEmail").val();
-
-//     $.ajax({
-//         type : "POST",
-//         url : "/sendMailAuth.json",
-//         dataType:'json',
-//         contentType : "application/json",
-//         data : JSON.stringify({
-//             mEmail : email,
-//             type : 'J'
-//         }),
-//         success : function(obj){
-//             //정상적으로 이메일이 발송되었을때에만, 이메일 인증번호 입력 창 생성
-//             if(obj.resultCd == 1){
-//                 $.alertSuccess(obj.resultMsg);
-//                 $('#mEmailChk').show();
-//                 $('#confirmAuthBtn').show();
-//             }else {
-//                 $.alertError(obj.resultMsg);
-//             }
-//         }
-//     });
-// }
-
-//이메일 인증번호 확인
-// function confirmMail(){
-//     let emailChk = $("#mEmailChk").val();
-//     let email = $("#mEmail").val();
-
-//     $.ajax({
-//         type : "POST",
-//         url : "/confirmMailAuth.json",
-//         dataType:'json',
-//         contentType : "application/json",
-//         data : JSON.stringify({
-//             mEmail : email,
-//             inputAuth : emailChk,
-//             type : 'J'
-//         }),
-//         success : function(obj){
-//             //정상적으로 이메일이 발송되었을때에만, 이메일 인증번호 입력 창 생성
-//             if(obj.resultCd == 1){
-//                 $.alertSuccess(obj.resultMsg);
-//                 $("#emailChk_info").data("dupl", "Y");
-//                 $("#emailChk_info").hide();
-//                 $("#mEmail").prop('readonly', true);
-//             }else {
-//                 $.alertError(obj.resultMsg);
-//             }
-//         }
-//     });
-// }
-
-//회원가입 절차
-function joinJson(){
-    if(fn_dataChk(formData)){
-        var inputName = document.getElementById("mName").value;
-        var inputId = document.getElementById("mId").value;
-        var inputPw = document.getElementById("mPw").value;
-        var inputNickname = document.getElementById("mNickname").value;
-        var inputEmail = document.getElementById("mEmail").value;
-        var inputBirth = document.getElementById("mBirth").value;
-        var inputBirthYn =  document.querySelector('input[name="mBirthYn"]:checked').value;
-        var inputPwChk = document.getElementById("mPwChk").value;
+        if(chkVal == ""){ $.alertError(idName + "을(를) 입력해주세요!! •̀ㅅ•́"); return;}
+        
+        if(type == 'id') ajaxData = {mId: chkVal, type:type}
+        else if(type == 'nickname') ajaxData = {mNickname: chkVal, type:type}
 
         $.ajax({
-            type : 'POST',
+            type : "POST",
             contentType : "application/json",
-            url : '/join.json',
-            data : JSON.stringify({
-                mName : inputName,
-                mId : inputId,
-                mPw : inputPw,
-                mNickname : inputNickname,
-                mEmail : inputEmail,
-                mBirth : inputBirth,
-                mBirthYn : inputBirthYn,
-                mPwChk : inputPwChk
-            }),
+            url : '/inputCheck.json',
+            data : JSON.stringify(ajaxData),
             dataType : 'json',
             success : function(rslt){
-                if(rslt.resultCd==='1'){
-                    //성공일시
-                    $.alertSuccess(rslt.resultMsg);
-                    location.href = rslt.resultUrl;
+                if(rslt.resultCd == 1){
+                    if(rslt.type == 'id'){
+                        $.alertSuccess("아이디 중복 검사 완료");
+                        
+                        $("#idChk_info").data("dupl", "Y");
+                        $("#idChk_info").hide();
+                    } else if(rslt.type == 'nickname'){
+                        $.alertSuccess("닉네임 중복 검사 완료");
+                        $("#nickChk_info").data("dupl", "Y");
+                        $("#nickChk_info").hide();
+                    }
                 } else {
-                    //실패일시
-                    $.alertError(rslt.resultMsg);
+                    $.alertError("이미 사용하고 있어요. ʅ（◞‿◟）ʃ");
                 }
             },
             error : function(request, status, error){
@@ -285,4 +185,56 @@ function joinJson(){
             }
         });
     }
-}
+
+    //회원가입 => 중복체크 후, 값 변경되면 다시 중복체크
+    function inputChk(type){
+        if(type == 'idDupl'){
+            $("#idChk_info").show();
+            $("#idChk_info").data("dupl", "N");
+        } else if(type == 'nick'){
+            $("#nickChk_info").show();
+            $("#nickChk_info").data("dupl", "N");
+        };
+    }
+
+    //회원가입 절차
+    function joinJson(){
+        if(fn_dataChk(formData)){
+            var inputName = document.getElementById("mName").value;
+            var inputId = document.getElementById("mId").value;
+            var inputPw = document.getElementById("mPw").value;
+            var inputNickname = document.getElementById("mNickname").value;
+            var inputEmail = document.getElementById("mEmail").value;
+            var inputBirth = document.getElementById("mBirth").value;
+            var inputBirthYn =  document.querySelector('input[name="mBirthYn"]:checked').value;
+            var inputPwChk = document.getElementById("mPwChk").value;
+
+            $.ajax({
+                type : 'POST',
+                contentType : "application/json",
+                url : '/join.json',
+                data : JSON.stringify({
+                    mName : inputName,
+                    mId : inputId,
+                    mPw : inputPw,
+                    mNickname : inputNickname,
+                    mEmail : inputEmail,
+                    mBirth : inputBirth,
+                    mBirthYn : inputBirthYn,
+                    mPwChk : inputPwChk
+                }),
+                dataType : 'json',
+                success : function(rslt){
+                    if(rslt.resultCd==='1'){
+                        $.alertSuccessRtn(rslt.resultMsg, rslt.resultUrl);
+                    } else {
+                        //실패일시
+                        $.alertError(rslt.resultMsg);
+                    }
+                },
+                error : function(request, status, error){
+                    $.alertError("javaScript error : "+ error + "request :" + request + "status : " + status);
+                }
+            });
+        }
+    }
