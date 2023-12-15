27e4f7010c4a3afd14d852e4c8c4448f0b2e8665
hyony590 <runa369@naver.com>
2023-12-15 18:00:09 +0900

    ㅎㅎ

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -7,20 +7,20 @@ const headerToolbar = {
 
 //캘린더 생성 옵션
 const calendarOption = {
-    height: '750px',                //높이 설정
-    initialView : 'dayGridMonth',   //초기 로드 될때 보이는 캘린더 화면 (기본 설정 : 달)
-    headerToolbar : headerToolbar,  //헤더 툴바 설정
-    locale: "ko",                   //한글로 변경
-    navLinks: true,                 //[월]과 [주]에서 해당 요일 클릭 시, [일]로 볼 수 있도록 이동함
-    editable: true,                 //일정을 들어서 옮기는 것이 가능
-    selectable : true,              //드래그하여 날짜범위 선택 가능
-    eventLimit : true,              //달력에 셀보다 많은 이벤트가 등록된 경우, 'more'로 표기함
-    weekends : true,                //초기에 주말 보이기
+    height: '750px',                                    //높이 설정
+    initialView : 'dayGridMonth',                       //초기 로드 될때 보이는 캘린더 화면 (기본 설정 : 달)
+    headerToolbar : headerToolbar,                      //헤더 툴바 설정
+    locale: "ko",                                       //한글로 변경
+    navLinks: true,                                     //[월]과 [주]에서 해당 요일 클릭 시, [일]로 볼 수 있도록 이동함
+    editable: true,                                     //일정을 들어서 옮기는 것이 가능
+    selectable : true,                                  //드래그하여 날짜범위 선택 가능
+    eventLimit : true,                                  //달력에 셀보다 많은 이벤트가 등록된 경우, 'more'로 표기함
+    weekends : true,                                    //초기에 주말 보이기
     events: [ 
                 {
                     title: 'All Day Event',
-                    start: '2023-11-05',
-                    end : '2023-11-08'
+                    start: '2023-12-15',
+                    end : '2023-12-17'
                 },
             ],
     //날짜 클릭 시, 이벤트 추가 
