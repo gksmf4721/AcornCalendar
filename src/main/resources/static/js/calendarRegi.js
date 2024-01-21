e0e262bfaaa8cf6c6745791bfefa4c6b2d1cd316
hyony590 <runa369@naver.com>
2024-01-21 18:11:22 +0900

    휴가 잔여일수에 따른 유효성

--- src/main/resources/static/js/calendarRegi.js
+++ src/main/resources/static/js/calendarRegi.js
@@ -117,68 +117,92 @@ function valueReset(type){
  * ************************************** */
 function regiEvent(type){
     //필수입력 검사 => 나중에 if문 안에 value값 체크하는것 집어넣기
-    // if(fn_dataChk(formData)){}
-    let inputcalSeq = document.getElementById("P_CalSeq").value;            //캘린더 시퀀스
-    let inputmSeq = document.getElementById("P_mSeq").value;                //일정 등록 회원 시퀀스
-    let inputcontCont = document.getElementById("P_contCont").innerHTML;    //일정 내용
-    let inputcontTitle = title.value;                                       //제목
-    let inputcontStartDt = startDate.value;                                 //시작 날짜
-    let inputcontEndDt = endDate.value;                                     //마감 날짜
-    let inputcontStartTm = startTime.value;                                 //일정 시작 시각
-    let inputcontEndTm = endTime.value;                                     //일정 마감 시각
-    let inputcalDetailType = document.getElementById("cal_category").value; //카테고리 종류
-    let inputcontAlldayYn = alldayCheck.checked == true ? "Y" : "N";        //종일 여부
-    let inputcontSeq = contSeq.value;
+    if(fn_dataChk(formData)){
+        let inputcalSeq = document.getElementById("P_CalSeq").value;            //캘린더 시퀀스
+        let inputmSeq = document.getElementById("P_mSeq").value;                //일정 등록 회원 시퀀스
+        let inputcontCont = document.getElementById("P_contCont").innerHTML;    //일정 내용
+        let inputcontTitle = title.value;                                       //제목
+        let inputcontStartDt = startDate.value;                                 //시작 날짜
+        let inputcontEndDt = endDate.value;                                     //마감 날짜
+        let inputcontStartTm = startTime.value;                                 //일정 시작 시각
+        let inputcontEndTm = endTime.value;                                     //일정 마감 시각
+        let inputcalDetailType = document.getElementById("cal_category").value; //카테고리 종류
+        let inputcontAlldayYn = alldayCheck.checked == true ? "Y" : "N";        //종일 여부
+        let inputcontSeq = contSeq.value;
+        let w_vact_cnt =  document.getElementById('w_vact_cnt').innerText;
 
-    //종일버튼 체크 시, 시작시각&종료시각 null로 넣기
-    if(inputcontAlldayYn == "Y"){
-        inputcontStartTm = '00:01:00.0';
-        inputcontEndTm = '00:01:00.0';
-    }
+        if(inputcalDetailType === 'S1'){
 
-    ajaxData = {
-        calSeq : inputcalSeq,
-        mSeq : inputmSeq,
-        contCont : inputcontCont,
-        contTitle : inputcontTitle,
-        contStartDt : inputcontStartDt,
-        contEndDt : inputcontEndDt,
-        contStartTm : inputcontStartTm,
-        contEndTm : inputcontEndTm,
-        calDetailType : inputcalDetailType,
-        contAlldayYn : inputcontAlldayYn,
-        contSeq : inputcontSeq
-    }
+            let startDate = new Date(inputcontStartDt);
+            let endDate = new Date(inputcontEndDt);
 
-    console.log("캘린더 저장할 때 넘기는 값::");
-    console.log(JSON.stringify(ajaxData));
+            var timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+            var dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1;
 
-    let ajaxUrl = "";
-    let text = "";
-    if(type == 'N'){
-        ajaxUrl = "/cont.json";
-        text = "저장되었습니다!"
-    } else {
-        //수정할 때 넘길 url만 추가하면 됨
-        ajaxUrl = "/contUpdate.json";
-        text = "수정되었습니다!";
-    }   
-    $.ajax({
-        type : "POST",
-        contentType : "application/json",
-        url : ajaxUrl,
-        data : JSON.stringify(ajaxData),
-        dataType : 'json',
-        success : function(rslt){
-            document.getElementById('w_vact_cnt').innerText = rslt.data;
-            $.alertSuccess(text);
-            calendarRender(inputcontStartDt);
-            modalSlide('', '', 'N');
-        },
-        error : function(request, status, error){
-            $.alertError("javaScript error : "+ error + "request :" + request + "status : " + status);
+            if(w_vact_cnt < dayDiff){
+                $.alertError('휴가 잔여일수가 없습니다.');
+                return;
+            }
+        } 
+        if(inputcalDetailType === 'S2' && w_vact_cnt - 0.5 < 0){
+            $.alertError('휴가 잔여일수가 없습니다.');
+            return;
         }
-    });
+    
+        //종일버튼 체크 시, 시작시각&종료시각 null로 넣기
+        if(inputcontAlldayYn == "Y"){
+            inputcontStartTm = '00:01:00.0';
+            inputcontEndTm = '00:01:00.0';
+        }
+    
+        ajaxData = {
+            calSeq : inputcalSeq,
+            mSeq : inputmSeq,
+            contCont : inputcontCont,
+            contTitle : inputcontTitle,
+            contStartDt : inputcontStartDt,
+            contEndDt : inputcontEndDt,
+            contStartTm : inputcontStartTm,
+            contEndTm : inputcontEndTm,
+            calDetailType : inputcalDetailType,
+            contAlldayYn : inputcontAlldayYn,
+            contSeq : inputcontSeq
+        }
+
+        console.log("캘린더 저장할 때 넘기는 값::");
+        console.log(JSON.stringify(ajaxData));
+
+        let ajaxUrl = "";
+        let text = "";
+        if(type == 'N'){
+            ajaxUrl = "/cont.json";
+            text = "저장되었습니다!"
+        } else {
+            //수정할 때 넘길 url만 추가하면 됨
+            ajaxUrl = "/contUpdate.json";
+            text = "수정되었습니다!";
+        }   
+        $.ajax({
+            type : "POST",
+            contentType : "application/json",
+            url : ajaxUrl,
+            data : JSON.stringify(ajaxData),
+            dataType : 'json',
+            success : function(rslt){
+                document.getElementById('w_vact_cnt').innerText = rslt.data;
+                document.getElementById('w_vact_cnt').value = rslt.data;
+                $.alertSuccess(text);
+                calendarRender(inputcontStartDt);
+                modalSlide('', '', 'N');
+            },
+            error : function(request, status, error){
+                $.alertError("javaScript error : "+ error + "request :" + request + "status : " + status);
+            }
+        });
+    }
+    
+
+    
 }
 
 function deltEvent(){
